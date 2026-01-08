de203313926bba72c522b17628ca1010b590fca2
kimminj0208 <kimminj0208@gmail.com>
2026-01-08 22:29:03 +0900

    마저 다했다. 뭔가 모자라서 문제 몇개 더 풀겠음.

--- 2026-01-07.py
+++ 2026-01-07.py
@@ -158,3 +158,8 @@ for i in range(n):
 #   print('+' * (2*i +1))
 
 
+#4장 마저 이어서 함.
+#play = None
+#while play not in ['가위', '바위', '보']:
+#    play = input('가위, 바위, 보 중에서 선택해')
+#print('선택한 값은 : ', play)
---
+++ 2026-01-08.py
@@ -0,0 +1,7 @@
+#4장 마저 이어서 함.
+#play = None
+#while play not in ['가위', '바위', '보']:
+#    play = input('가위, 바위, 보 중에서 선택해')
+#print('선택한 값은 : ', play)
+
+
